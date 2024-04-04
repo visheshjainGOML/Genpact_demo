bfedc874e45d9e3740742f4346e2db71ab4f0e54
Hxtreme <vishesh.jain@goml.io>
2024-04-05 00:39:59 +0530

    commit6

--- app.py
+++ app.py
@@ -94,7 +94,7 @@ async def book_slot(booking_request: BookingRequest, conn: psycopg2.extensions.c
         cur.close()
 
 def run_server():
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
  
 if __name__ == "__main__":
     run_server()
