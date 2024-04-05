f9dddcfbed962af39eed4099bdbf5691d3350bf1
Hxtreme <vishesh.jain@goml.io>
2024-04-05 14:06:18 +0530

    added cors 2

--- app.py
+++ app.py
@@ -9,12 +9,15 @@ import uvicorn
 from datetime import datetime, timedelta
 
 app = FastAPI()
+origins = [
+   "http://localhost:8080"
+]
 app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["GET", "OPTIONS", "POST", "PUT", "DELETE"],
-    allow_headers=["*"],
+   CORSMiddleware,
+   allow_origins=origins,
+   allow_credentials=True,
+   allow_methods=["*"],
+   allow_headers=["*"],
 )
 
 def get_db_connection():
